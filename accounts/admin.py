6271f1c7943fe06a07956cbc22337ed23a900e7f
DualOverhead <32436206+DualOverhead@users.noreply.github.com>
2020-10-04 13:14:45 -0700

    Add files via upload

---
+++ accounts/admin.py
@@ -0,0 +1,11 @@
+# from django.contrib import admin
+# from django.contrib.auth.models import User
+
+
+# class MyUserAdmin(admin.ModelAdmin):
+#     list_display = ['email', 'first_name',
+#                     'last_name', 'is_active', 'last_login']
+
+
+# admin.site.unregister(User)
+# admin.site.register(User, MyUserAdmin)
---
+++ accounts/views.py
@@ -0,0 +1,8 @@
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import UserCreateForm
+
+class SignUpView(generic.CreateView):
+    form_class=UserCreateForm
+    success_url=reverse_lazy('login')
+    template_name='signup.html'
